0f18c78998c4777d31c97504a821ee097de395e4
LyamBRS <cous5830@gmail.com>
2023-10-25 14:58:38 -0400

    Created Status.hpp where SafeBox's status will be stored as an enum

---
+++ XFactor/include/SafeBox/Status.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file Status.hpp
+ * @author LyamBRS (lyam.brs@gmail.com)
+ * @brief File which contains the definitions of
+ * SafeBox's status which is used during comms
+ * with it.
+ * @version 0.1
+ * @date 2023-10-25
+ * @copyright Copyright (c) 2023
+ */
+
+#pragma once
