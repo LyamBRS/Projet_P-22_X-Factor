1f5ba352c1e24cef8bbd45a9c68f8546fefc6157
LyamBRS <cous5830@gmail.com>
2023-10-26 14:15:44 -0400

    Created Package.hpp

---
+++ XFactor/include/Package/Package.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file Package.hpp
+ * @author LyamBRS (ltam.brs@gmail.com)
+ * @brief
+ * File containing the header definitions of the
+ * various functions that are used to locate,
+ * identify, pick up, handle, and deposite a
+ * package for XFactor. 
+ * @version 0.1
+ * @date 2023-10-26
+ * @copyright Copyright (c) 2023
+ */
