c0f1d9b7cddcf5742ad98355db2eb2fa9dbb2d86
LyamBRS <cous5830@gmail.com>
2023-10-30 12:02:21 -0400

    Added functions in Accelerometer.hpp

--- XFactor/include/Sensors/Accelerometer/Accelerometer.hpp
+++ XFactor/include/Sensors/Accelerometer/Accelerometer.hpp
@@ -13,4 +13,73 @@
 
 #pragma region [FUNCTIONS]
 
+/**
+ * @brief 
+ * Initialises an accelerometer to be used on
+ * XFactor. The name of the accelerometer will
+ * define how these functions are called.
+ * @param accelerometerPin 
+ * @return true:
+ * Successfully initialised the accelerometer
+ * using the specifed pins.
+ * @return false:
+ * Failed to initialise the accelerometer.
+ */
+bool Accelerometer_Innit(int accelerometerPin);
+
+/**
+ * @brief
+ * Function that returns the X value of the
+ * accelerometer using functions from an arduino
+ * library. The value is raw and not converted.
+ * @param accelerometerPin:
+ * The pin of the accelerometer that was
+ * initialised
+ * @return float:
+ * If 0 is returned, the function failed to
+ * execute properly. 
+ */
+float Accelerometer_GetX(int accelerometerPin);
+
+/**
+ * @brief
+ * Function that returns the Y value of the
+ * accelerometer using functions from an arduino
+ * library. The value is raw and not converted.
+ * @param accelerometerPin:
+ * The pin of the accelerometer that was
+ * initialised
+ * @return float:
+ * If 0 is returned, the function failed to
+ * execute properly. 
+ */
+float Accelerometer_GetY(int accelerometerPin);
+
+/**
+ * @brief
+ * Function that returns the Z value of the
+ * accelerometer using functions from an arduino
+ * library. The value is raw and not converted.
+ * @param accelerometerPin:
+ * The pin of the accelerometer that was
+ * initialised
+ * @return float:
+ * If 0 is returned, the function failed to
+ * execute properly. 
+ */
+float Accelerometer_GetZ(int accelerometerPin);
+
+/**
+ * @brief
+ * Function that returns the compass value of the
+ * accelerometer using functions from an arduino
+ * library. The value is raw and not converted.
+ * @param accelerometerPin:
+ * The pin of the accelerometer that was
+ * initialised
+ * @return float:
+ * If 0 is returned, the function failed to
+ * execute properly. 
+ */
+float Accelerometer_GetCompass(int accelerometerPin);
 #pragma endregion
