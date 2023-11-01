1beb9a9436bd74381c6bbaf462dba4071eece411
Lyam[BRS] <cous5830@gmail.com>
2023-11-01 18:13:44 -0400

    Placed the end of project code in main.cpp

--- XFactor/src/main.cpp
+++ XFactor/src/main.cpp
@@ -1,18 +1,25 @@
-
 /**
+ * @file main.cpp
+ * @author LyamBRS (lyam.brs@gmail.com)
  * @brief
- * Arduino's setup function
+ * Arduino's main file.
+ * @version 0.1
+ * @date 2023-11-01
+ * @copyright Copyright (c) 2023
  */
+
+// - INCLUDES -//
+#include "Actions/Actions.hpp"
+#include "XFactor/Init.hpp"
+
+/// @brief Arduino's initialisation function.
 void setup()
 {
-
+  XFactor_Innit();
 }
 
-/**
- * @brief
- * while(1) equivalent
- */
+/// @brief Arduino's while(1) function.
 void loop()
 {
-
+  Execute_CurrentFunction();
 }
