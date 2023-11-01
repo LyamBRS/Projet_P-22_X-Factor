3a3c3cfbf7e161543e9941b104432fb2bff0cf52
Lyam[BRS] <cous5830@gmail.com>
2023-11-01 18:13:32 -0400

    Made status.cpp compilable

--- XFactor/src/XFactor/Status.cpp
+++ XFactor/src/XFactor/Status.cpp
@@ -1,110 +1,21 @@
 /**
- * @file Status.hpp
+ * @file Status.cpp
  * @author LyamBRS (lyam.brs@gmail.com)
  * @brief
- * File containing the header definitions of
+ * File containing the code definitions of
  * functions used to handle XFactor's status and
  * store as well as use the status.
  * @version 0.1
- * @date 2023-10-26
+ * @date 2023-11-01
  * @copyright Copyright (c) 2023
  */
 
-#pragma once
-
-#pragma region [ENUM]
-
-/**
- * @brief Enumeration of all the possible status
- * that XFactor can have. Each status represents
- * a mode or activity that XFactor is currently
- * doing. These modes are used for SafeBox to
- * take decisions and communicate properly.
- * 
- * @attention
- * Usage: variable = XFactor_StatusEnum::Off;
- * @warning
- * Usage: XFactorStatus = XFactor_StatusEnum::Alarm;
- */
-enum XFactor_StatusEnum {
-
-    /// @brief Default status when the program fails to communicate properly.
-    CommunicationError = -1,
-
-    /// @brief XFactor is off. Default status.
-    Off = 0,
-
-    /// @brief Status used when XFactor awaits for SafeBox to give it the go
-    WaitingForDelivery = 1,
-
-    /// @brief Satus used when XFactor is leaving the garage
-    LeavingSafeBox = 2,
-
-    /// @brief Status used when XFactor is preparing to search for the package
-    PreparingForTheSearch = 3,
-
-    /// @brief Status used when XFactor is actively searching for a package
-    SearchingForAPackage = 4,
-
-    /// @brief Status used when XFactor is examinating a potential package
-    ExaminatingAPackage = 5,
-
-    /// @brief Status used when XFactor is actively picking up or trying to pick up a package
-    PickingUpAPackage = 6,
-
-    /// @brief Status used when XFactor is trying to calculate a way home
-    CalculatingRouteHome = 7,
-
-    /// @brief Status used when XFactor is actively returnin home
-    ReturningHome = 8,
-
-    /// @brief Status used when XFactor is preparing to drop off a package inside of SafeBox
-    PreparingForDropOff = 9,
-
-    /// @brief Status used when XFactor is actively dropping the package inside SafeBox
-    DroppingOff = 10,
-
-    /// @brief Status used when XFactor is attempting to confirm that the package was dropped off
-    ConfirmingDropOff = 11,
-
-    /// @brief Status used whenn XFactor is trying to enter the garage
-    EnteringSafeBox = 12,
-
-    /// @brief Status used when XFactor fails to examine if a potential package is a package or not.
-    PackageExaminationFailed = 13,
-
-    /// @brief Status used when XFactor cannnot pick up an identified package after X attempts
-    PackagePickUpFailed = 14,
-
-    /// @brief Statys used when XFactor fails to drop off the package inside SafeBox.
-    PackageDropOffFailed = 15,
-
-    /// @brief Status used when XFactor failed to find a package that was delivered
-    NoPackageFound = 16,
-
-    /// @brief Safebox is in maintenance mode
-    Maintenance = 50,
-    
-    /// @brief SafeBox encountered a critical error
-    Error = 51,
-
-    /// @brief SafeBox has sounded the alarm and won't stop until the user disarms it.
-    Alarm = 255
-};
-
-/**
- * @brief Global variable used to store the last
- * status that was retreived from SafeBox.
- * Declared as extern in @ref Status.hpp but
- * initialised in @ref SafeBox_Innit. 
- */
-extern int XFactorStatus;
-
-#pragma endregion
+// - INCLUDES - //
+#include "XFactor/Status.hpp"
 
 #pragma region [FUNCTIONS]
 /**
- * @brief 
+ * @brief
  * Function that sets the global variable which
  * holds the current status of XFactor to a new
  * desired value from @ref XFactor_StatusEnum
@@ -117,19 +28,25 @@ extern int XFactorStatus;
  * The specified status does not match available
  * status of @ref XFactor_StatusEnum
  */
-bool XFactor_SetNewStatus(unsigned char newStatus);
+bool XFactor_SetNewStatus(unsigned char newStatus)
+{
+    return false;
+}
 
 /**
- * @brief 
+ * @brief
  * Function that returns the current status of
- * XFactor as an unsigned char. You must check 
+ * XFactor as an unsigned char. You must check
  * that value with @ref XFactor_StatusEnum to
  * identify what the current status of XFactor is
- * 
+ *
  * @return unsigned char
- * value from @ref XFactor_StatusEnum which 
+ * value from @ref XFactor_StatusEnum which
  * corresponds to XFactor's current status.
  */
-unsigned char XFactor_GetStatus();
+unsigned char XFactor_GetStatus()
+{
+    return XFactorStatus;
+}
 
 #pragma endregion
