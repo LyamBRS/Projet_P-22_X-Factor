34bfe51bc987df3eeaa00523247369e7040a33df
Lyam[BRS] <cous5830@gmail.com>
2023-11-10 21:40:00 -0500

    Updated Status of SafeBox in XFactor

--- XFactor/include/SafeBox/Status.hpp
+++ XFactor/include/SafeBox/Status.hpp
@@ -1,22 +1,24 @@
 /**
  * @file Status.hpp
  * @author LyamBRS (lyam.brs@gmail.com)
- * @brief File which contains the definitions of
- * SafeBox's status which is used during comms
- * with it.
+ * @brief
+ * File containing the header definitions of
+ * functions used to handle SafeBox's status and
+ * store as well as use the status.
  * @version 0.1
- * @date 2023-10-25
+ * @date 2023-11-10
  * @copyright Copyright (c) 2023
  */
 
 #pragma once
 
-// #pragma region [Enumerations]
+//#pragma region [ENUM]
+
 /**
  * @brief Enumeration of all the possible status
  * that SafeBox can have. Each status represents
  * a mode or activity that SafeBox is currently
- * ongoing. These modes are used for XFactor to
+ * doing. These modes are used for SafeBox to
  * take decisions and communicate properly.
  *
  * @attention
@@ -26,29 +28,29 @@
  */
 enum SafeBox_StatusEnum {
 
-    /// @brief Default status when the program fails to retrieve SafeBox's status.
-    CommunicationError = -1,
+    /// @brief Default status when the program fails to communicate properly.
+    CommunicationError = 253,
 
-    /// @brief SafeBox is off
+    /// @brief SafeBox is off. Default status.
     Off = 0,
 
-    /// @brief Safebox is waiting for the user to input package number and other informations. Its disarmed.
-    WaitingAfterConfiguration = 1,
+    /// @brief Status used when SafeBox awaits the doorbell to be read to give it the go
+    WaitingForDelivery = 1,
 
-    /// @brief Safebox is currently being configurated by its user. Its disarmed.
-    BeingConfigurated = 2,
+    /// @brief Status used when SafeBox is waiting for XFactor to retrieve a package.
+    WaitingForRetrieval = 2,
 
-    /// @brief Safebox is armed and waiting for a doorbell to start the procedure
-    WaitingForDoorbell = 3,
+    /// @brief Status used when SafeBox is waiting for XFactor to return to SafeBox
+    WaitingForReturn = 3,
 
-    /// @brief Safebox identified that a package was delivered. XFactor needs to retrieve it. Its armed.
-    PackageDelivered = 4,
+    /// @brief Status used when SafeBox is ready to handle a drop off by XFactor
+    ReadyForDropOff = 4,
 
-    /// @brief Safebox is waiting for XFactor to deposit its package inside of it
-    WaitingForDeposit = 5,
+    /// @brief Status used when SafeBox is unlocked
+    Unlocked = 5,
 
-    /// @brief Safebox is waiting for XFactor to return inside of it.
-    WaitingForXFactor = 6,
+    /// @brief Status used when SafeBox is actively dropping the package inside SafeBox
+    DroppingOff = 10,
 
     /// @brief Safebox is in maintenance mode
     Maintenance = 50,
@@ -56,15 +58,43 @@ enum SafeBox_StatusEnum {
     /// @brief SafeBox encountered a critical error
     Error = 51,
 
+    /// @brief Safebox is waiting for XFactor to be online.
+    WaitingForXFactor = 254,
+
     /// @brief SafeBox has sounded the alarm and won't stop until the user disarms it.
     Alarm = 255
 };
 
+//#pragma endregion
+
+//#pragma region [FUNCTIONS]
 /**
- * @brief Global variable used to store the last
- * status that was retrieved from SafeBox.
- * Declared as extern in @ref Status.hpp but
- * initialised in @ref SafeBox_Init.
+ * @brief
+ * Function that sets the global variable which
+ * holds the current status of SafeBox to a new
+ * desired value from @ref SafeBox_StatusEnum
+ * @param newStatus
+ * A value within @ref SafeBox_StatusEnum
+ * @return true:
+ * The status was successfully updated to the new
+ * specified status.
+ * @return false:
+ * The specified status does not match available
+ * status of @ref SafeBox_StatusEnum
  */
-extern int SafeBoxStatus;
-// #pragma endregion
+bool SafeBox_SetNewStatus(unsigned char newStatus);
+
+/**
+ * @brief
+ * Function that returns the current status of
+ * SafeBox as an unsigned char. You must check
+ * that value with @ref SafeBox_StatusEnum to
+ * identify what the current status of SafeBox is
+ *
+ * @return unsigned char
+ * value from @ref SafeBox_StatusEnum which
+ * corresponds to SafeBox's current status.
+ */
+unsigned char SafeBox_GetStatus();
+
+//#pragma endregion
