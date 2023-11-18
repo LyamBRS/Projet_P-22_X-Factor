479ea2ab79a9bba084beb9fc184efe4719caf389
Lyam[BRS] <cous5830@gmail.com>
2023-11-18 14:34:10 -0500

    Fixed grammar in Accelerometer

--- XFactor/include/Sensors/Accelerometer/Accelerometer.hpp
+++ XFactor/include/Sensors/Accelerometer/Accelerometer.hpp
@@ -19,15 +19,15 @@
 //#define BAUD_RATE 9600
 #define MPU6050_CLOCK_SPEED 400000           // 400kHz I2C clock
 #define MPU6050_ADDRESS_AD0_LOW 0x68 // MPU6050 low I2c address
-#define ACCEL_XOUT_H 0x3B            // Accelometer first high register
+#define ACCELEROMETER_XOUT_H 0x3B            // Accelerometer first high register
 #define GYRO_XOUT_H 0x43             // Gyroscope first high register
-#define ACCEL_CONFIG 0x1C            // Accelometer configuration register
+#define ACCELEROMETER_CONFIG 0x1C            // Accelerometer configuration register
 #define GYRO_CONFIG 0x1B             // Gyroscope configuration register
 #define PWR_MGMT_1 0X6B              // Power management 1 register
 
 // TODO: make this as a function of the scale, use AFS_SEL registers
-#define ACCELOMETER_SENSIVITY 16384.0f //  in deg/s for a scale of +- 2g, see the datasheet
-#define GYROSCOPE_SENSIVITY 131.0f     //  in g for scale of +- 250 deg/s, see the datasheet
+#define ACCELEROMETER_SENSIVITY 16384.0f //  in deg/s for a scale of +- 2g, see the data sheet
+#define GYROSCOPE_SENSIVITY 131.0f     //  in g for scale of +- 250 deg/s, see the data sheet
 #define MPU6050_DATA_SIZE 6                    // Read 6 values in total, each axis value is stored in 2 registers
 
 // uncomment this to calibrate the sensor
@@ -37,7 +37,7 @@
  * @brief enum containing all the possible scales
  * that the accelerometer can have. Used to send
  * on Wire (i2c/spi)
- * 
+ *
  * @warning
  * DO NOT USE OUTSIDE OF ACCELEROMETER.CPP
  */
@@ -47,13 +47,13 @@ typedef enum
     FOUR_G = 0x01,
     EIGHT_G = 0x10,
     SIXTEEN_G = 0x11
-} accelometer_scale;
+} accelerometer_scale;
 
 /**
  * @brief enum containing all the possible scales
  * that the gyro can have. Used to send
  * on Wire (i2c/spi)
- * 
+ *
  * @warning
  * DO NOT USE OUTSIDE OF ACCELEROMETER.CPP
  */
@@ -68,7 +68,7 @@ typedef enum
 /**
  * @brief enum containing all the axes of the
  * accelerometer. Used on Wire (i2c/spi)
- * 
+ *
  * @warning
  * DO NOT USE OUTSIDE OF ACCELEROMETER.CPP
  */
@@ -143,25 +143,25 @@ float Accelerometer_GetZ();
 float Accelerometer_GetCompass();
 
 // Offset calibration function
-// CAUSTION: place the IMU flat in order to get the proper values
-void calculate_IMU_error(unsigned correctionCount);
+// CAUTION: place the IMU flat in order to get the proper values
+void Accelerometer_CalculateIMUError(unsigned correctionCount);
 
 /**
  * @brief
  * Sets the axis scales of the accelerometer.
  * DO NOT USE OUTSIDE OF ACCELEROMETER.CPP
- * 
- * Scales are defined in the datasheet
- * @param scale 
+ *
+ * Scales are defined in the data sheet
+ * @param scale
  */
-void set_accelometer_scale(accelometer_scale scale);
+void Accelerometer_SetScale(accelerometer_scale scale);
 
 /**
  * @brief
  * Sets the axis scales of the gyro.
  * DO NOT USE OUTSIDE OF ACCELEROMETER.CPP
- * 
- * Scales are defined in the datasheet
- * @param scale 
+ *
+ * Scales are defined in the data sheet
+ * @param scale
  */
-void set_gyro_scale(gyro_scale scale);
+void Accelerometer_SetGyro(gyro_scale scale);
