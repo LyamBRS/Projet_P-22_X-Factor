70fdbc0e3ea3426b9735cb80a028a8afd1ba765f
LyamBRS <cous5830@gmail.com>
2023-11-23 16:34:13 -0500

    Bunch of changes I cant be bothered to detail

--- XFactor/src/Actions/Utils.cpp
+++ XFactor/src/Actions/Utils.cpp
@@ -34,7 +34,11 @@ int ExecutionUtils_StatusCheck(int currentExecutionFunctionId)
       case SafeBox_Status::WaitingForDelivery:
         return FUNCTION_ID_WAIT_FOR_DELIVERY;
       case SafeBox_Status::Alarm:
+        Debug_Error("Utils", "ExecutionUtils_StatusCheck", "SAFEBOX IS IN ALARM");
         return FUNCTION_ID_ALARM;
+      case SafeBox_Status::Error:
+        Debug_Error("Utils", "ExecutionUtils_StatusCheck", "SAFEBOX IS IN ERROR");
+        return FUNCTION_ID_ERROR;
       default:
         return currentExecutionFunctionId;
     }
